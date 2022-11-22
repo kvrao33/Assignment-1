872bc0565b28007f84f7635838febecfc8cbe1f6
Karthik V rao <karthik.rao@niveussolutions.com>
2022-11-22 14:15:33 +0530

    create the promise

--- a1.js
+++ a1.js
@@ -1,1 +1,14 @@
-console.log("hello")
+let varPromise  = new Promise((resolve,reject)=>{
+    let course=50;
+    if(course<50){
+        reject({message:"You should complete 50% of your course"})
+    }else{
+        resolve({message:"Congrats now your eligible for get into Project"})
+    }
+})
+
+varPromise.then((data)=>{
+    console.log(data.message);
+}).catch(data=>{
+    console.log(data.message);
+})
