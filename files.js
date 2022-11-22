ca93f05f26a5d3ce732dd31d578e2e5eaf2d465c
Karthik V rao <karthik.rao@niveussolutions.com>
2022-11-22 15:13:24 +0530

    create file using async function

--- files.js
+++ files.js
@@ -1,5 +1,14 @@
-let fs= require('fs');
-fs.writeFile("newfile.txt","New file was created",(err)=>{
-    if(err) throw err;
-    console.log("file created successfully");
-})
+let fs= require('fs').promises;
+async function create(){
+
+try{
+
+  await fs.writeFile("newfile.txt","New file was created")
+  console.log("File created Successfully")
+}
+catch(err){
+    console.log("Error",err);
+}
+}
+create()
+    
